33030c7a676c0aae13a280120ad920359a2013c9
Apurv_Singh <Apurv_Singh@Dell.com>
2017-10-10 20:24:34 +0530

    Classes and class attributes and self object

--- Code/OOP/Classes.py
+++ Code/OOP/Classes.py
@@ -2,9 +2,24 @@ student_list = []
 
 
 class Student:
+
+    schoolName = "SJA"
     def __init__(self, name, id=0):
-        student_details = {"name": name, " id": id}
-        student_list.append(student_details)
+        self.name = name
+        self.id = id
+        student_list.append(self)
+        print(self.getSchool())
+
+    def __str__(self):
+        return "This overrides the memory instance message for student :" + self.name
+
+    def getNameCapitalize(self):
+        return self.name.capitalize()
+
+    def getSchool(self):
+        return self.schoolName
 
 student = Student("Apurv", 22)
-print(student_list)
+
+print(student)
+print(Student.schoolName)
